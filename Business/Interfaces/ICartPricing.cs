139868348c146789beea6dee06c705c290f1bf47
smdaghi <129605085+smdaghi@users.noreply.github.com>
2023-04-16 17:40:41 +0200

    feat(addingCartPricing): adding cart pricing - Ticket-3
    feat(addingCartPricing): adding cart pricing - Ticket-3

---
+++ Business/Interfaces/ICartPricing.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Domain.Entities;
+
+namespace Business.Interfaces
+{
+	public interface ICartPricing
+	{
+        public decimal TotalCartPrice(Cart cart);
+    }
+}
---
+++ Business/Services/CartPricing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Domain.Entities;
+
+namespace Business.Services
+{
+	public class CartPricing
+	{
+        public decimal TotalCartPrice(Cart cart)
+        {
+            return cart.GetTotalPrice();
+        }
+    }
+}
+
