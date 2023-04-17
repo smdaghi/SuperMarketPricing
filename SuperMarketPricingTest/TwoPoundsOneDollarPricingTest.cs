7f8dcc37ff0343aa82fb294481a1dc7e2f80549a
smdaghi <129605085+smdaghi@users.noreply.github.com>
2023-04-17 10:40:56 +0200

    feat(addingTwoPoundsOneDollarPricingTest): adding two pounds for one dollar pricing test - Ticket-21
    feat(addingTwoPoundsOneDollarPricingTest): adding two pounds for one dollar pricing test - Ticket-21

---
+++ SuperMarketPricingTest/TwoPoundsOneDollarPricingTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Services;
+
+namespace SuperMarketPricingTest
+{
+    [TestClass]
+    public class TwoPoundsOneDollarPricingTest
+	{
+        [TestMethod]
+        public void TwoPoundsOneDollarPricingUnitTest()
+		{
+            IProductPricing productPricing = new TwoPoundsOneDollarPricing();
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "Candy",
+                Weight = 20,
+                Unit = UnitEnum.Ounces
+            };
+            OrderLine order = new OrderLine(productPricing)
+            {
+                Id = 1,
+                Product = product
+            };
+            //Act
+            decimal price = order.GetPrice();
+            //Assert
+            Assert.AreEqual(0.625M, price);
+        }
+
+        [TestMethod]
+        public void TwoPoundsOneDollarPricingCardTest()
+        {
+            IProductPricing productPricing = new TwoPoundsOneDollarPricing();
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "Candy",
+                Weight = 20,
+                Unit = UnitEnum.Ounces
+            };
+            OrderLine order = new OrderLine(productPricing)
+            {
+                Id = 1,
+                Product = product
+            };
+            Cart cart = new Cart
+            {
+                OrderLines = new List<OrderLine>()
+            };
+            //
+            cart.OrderLines.Add(order);
+            //Act
+            decimal price = cart.GetTotalPrice();
+            //Assert
+            Assert.AreEqual(0.625M, price);
+        }
+    }
+}
+
